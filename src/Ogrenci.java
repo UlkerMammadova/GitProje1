c9752b09fc8543bf760ea4e1df7cbc980f40077f
Ulker Mammadova <ulker283@gmail.com>
2022-11-22 12:48:21 +0400

    ogrenci class ikinci islemler yapildi

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,6 +1,7 @@
 public class Ogrenci {
     public static void main(String[] args) {
 
-        System.out.println(" Merhaba dunya");
+        System.out.println(" Merhaba dunya2");
+        System.out.println(" Merhaba dunya2");
     }
 }
