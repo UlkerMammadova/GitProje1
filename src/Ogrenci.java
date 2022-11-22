241a3b690c5a4e2c01f2b6755d82db61187e400f
Ulker Mammadova <ulker283@gmail.com>
2022-11-22 12:54:05 +0400

    ogrenci class dorduncu islemler yapildi

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,7 +1,7 @@
 public class Ogrenci {
     public static void main(String[] args) {
 
-        System.out.println(" Merhaba dunya3");
-        System.out.println(" Merhaba dunya4");
+        System.out.println(" Panah");
+        System.out.println(" Ulkar");
     }
 }
