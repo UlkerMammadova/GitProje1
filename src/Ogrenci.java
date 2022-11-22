75a3d415dc941f962b9d5ec62ecd395d2abb8d27
Ulker Mammadova <ulker283@gmail.com>
2022-11-22 16:20:34 +0400

    ogrenci class sekkizinci islemler yapildi

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,7 +1,6 @@
 public class Ogrenci {
     public static void main(String[] args) {
 
-        System.out.println(" Davud");
-        System.out.println(" Madinasr");
+        System.out.println(" Panah Mammadov");
     }
 }
